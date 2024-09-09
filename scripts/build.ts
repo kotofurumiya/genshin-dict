bbc9b77bda3828336e997b81972e1951c5f800c1
Yuto Tanaka <karaage_nennen@icloud.com>
2024-09-09 22:13:54 +0900

    feat: add skk dict generator

--- scripts/build.ts
+++ scripts/build.ts
@@ -2,7 +2,14 @@ import fs from 'fs';
 import url from 'url';
 import path from 'path';
 import { loadDictList } from '../worddata/index.js';
-import { toKotoeriDict, toMacUserDict, toWindowsImeDict, expandVuHiragana, toUtf16BOM } from './lib/platform.js';
+import {
+  toKotoeriDict,
+  toMacUserDict,
+  toWindowsImeDict,
+  expandVuHiragana,
+  toUtf16BOM,
+  toSkkDict,
+} from './lib/platform.js';
 import { generateDocs } from './lib/docgen.js';
 import { DictItem } from '../worddata/dict.js';
 
@@ -14,6 +21,7 @@ const distDir = path.join(dirname, '..', 'genshin-dictionary');
 const winDictFile = path.join(distDir, '原神辞書_Windows.txt');
 const macDictFile = path.join(distDir, '原神辞書_macOS.txt');
 const macUserDictFile = path.join(distDir, '原神辞書_macOS_ユーザ辞書.plist');
+const skkDictFile = path.join(distDir, 'SKK-JISYO.genshin');
 
 console.log('辞書データを構築しています...');
 
@@ -28,6 +36,7 @@ console.log('辞書データを構築しています...');
   const winIme = toWindowsImeDict(words);
   const kotoeri = toKotoeriDict(words);
   const plist = toMacUserDict(words);
+  const skk = toSkkDict(words);
 
   console.log('ドキュメントを生成しています...');
 
@@ -44,9 +53,11 @@ console.log('辞書データを構築しています...');
   fs.writeFileSync(winDictFile, toUtf16BOM(winIme));
   fs.writeFileSync(macDictFile, kotoeri, 'utf8');
   fs.writeFileSync(macUserDictFile, plist, 'utf8');
+  fs.writeFileSync(skkDictFile, skk, 'utf8');
 
   console.log('完了しました');
   console.log(winDictFile);
   console.log(macDictFile);
   console.log(macUserDictFile);
+  console.log(skkDictFile);
 })();
